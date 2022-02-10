360bf938bdbd0bac377196bc23b9174b177930b5
Seth Coleman <colemans@moravian.edu>
2022-02-10 08:57:09 -0500

    Added more docs

--- grades.py
+++ grades.py
@@ -1,8 +1,9 @@
 
 def compute_hw_average(grades):
-	"""
-	Compute the average grade
-	"""
+    """
+    Compute the average grade
+    More Stuff
+    """
     if len(grades) == 0:
         return 0
     return grades[0]
